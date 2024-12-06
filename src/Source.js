a94b5d24bc5a20e3a9653455bc1e54883c28099b
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2024-12-06 11:01:32 +0300

    Added a margin-top on footer.

--- src/Source.js
+++ src/Source.js
@@ -3,7 +3,7 @@ import './App.css';
 function Source() {
   return (
     <div className="App">
-   <footer>
+   <footer className='footer'>
     <a href="https://famous-granita-d9883c.netlify.app" target='-blank'>Open-sourced code,</a> by <a href="https://github.com/FlorenceNjeriM" target='-blank'> Florence Njeri</a> from <a href="https://github.com/FlorenceNjeriM/my-app.git" target='-blank'> Github</a>
   </footer>    
     </div>
